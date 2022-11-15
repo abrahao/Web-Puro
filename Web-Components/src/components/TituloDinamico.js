d373413b5bdb5d96a5ae370906ce5fd799035a83
Abrahao Eneias <abrahao.eneias@dce.ufpb.br>
2022-11-15 16:37:04 -0300

    component titulo-dinamico atualizado

--- Web-Components/src/components/TituloDinamico.js
+++ Web-Components/src/components/TituloDinamico.js
@@ -2,16 +2,16 @@ class TituloDinamico extends HTMLElement{
 
     constructor(){
         super();
-        const shadow = this.attachShadow({mode: 'open'});
+        const shadow = this.attachShadow({mode: "open"});
         
-        const componentRoot = document.createElement('h1');
-        componentRoot.textContent = 'Abrahão';
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = this.getAttribute("titulo");
 
         //estilo
         const style = document.createElement('style');
         style.textContent = `
             h1{
-                color: red;
+                color: black;
             }
         `;
 
